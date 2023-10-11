d00b77a64d580826f2ea381abaafd52edd4789fc
smtamme <stellamariitamme@gmail.com>
2023-10-11 15:49:57 +0300

    lesson 23-10-11, next.js app

---
+++ app/api/todo/route.jsx
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json('hi')
+}
+
+export async function POST(req) {
+  const data = await req.json()
+
+  return NextResponse.json(data.hello)
+}
